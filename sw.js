0491f9462f386dc2acbe28f03d28fb77e16fc8bb
JotaSprout <jotasprout@gmail.com>
2018-06-03 10:31:19 -0400

    SW dynamically caches and serves images

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+const restImgCache = 'rest-img';
+
 self.addEventListener('install', function(event) {
     event.waitUntil(
         // stores stuff in the cache
@@ -17,8 +19,12 @@ self.addEventListener('install', function(event) {
     )
 });
 
-
 self.addEventListener('fetch', function(event) {
+    let requestUrl = new URL(event.request.url);
+    if (requestUrl.pathname.startsWith('/img/')) {
+        event.respondWith(showImg(event.request));
+        return;
+    }
     event.respondWith(
         caches.match(event.request).then(function(response) {
             if (response) {
@@ -28,3 +34,19 @@ self.addEventListener('fetch', function(event) {
         })
     );
 });
+
+function showImg(request) {
+    let localImgName = request.url.replace(/-\d+px\.jpg$/, '');
+
+    return caches.open(restImgCache).then(function(cache) {
+        return cache.match(localImgName).then(function(response) {
+            if (response) {
+                return response;
+            }
+            return fetch(request).then(function(networkResponse) {
+                cache.put(localImgName, networkResponse.clone());
+                return networkResponse;
+            });
+        });
+    });
+}
