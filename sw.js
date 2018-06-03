5e29c64d39783a6788800ada682691d5d5355d06
JotaSprout <jotasprout@gmail.com>
2018-06-03 14:35:49 -0400

    Trimming cache, commenting out breadcrumbs, and adding skip link

--- sw.js
+++ sw.js
@@ -6,11 +6,9 @@ self.addEventListener('install', function(event) {
         caches.open('restaurant').then(function(cache) {
             return cache.addAll([
                 './',
-                'index.html',
                 'js/dbhelper.js',
                 'js/main.js',
                 'js/restaurant_info.js',
-                'img/',
                 'restaurant.html',
                 'CSS/styles.css',
                 'data/restaurants.json'
