9e8cc857c7e53832e735c5677dfa383fc48f038e
JotaSprout <jotasprout@gmail.com>
2018-06-09 21:57:36 -0400

    Fixing Formatting

--- js/main.js
+++ js/main.js
@@ -146,7 +146,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = DBHelper.altTextForRestaurantImage(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
