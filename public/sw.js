3b3d60e2a739dd214ed6e455ffa414462131b5fe
JotaSprout <jotasprout@gmail.com>
2018-08-18 16:49:31 -0400

    Just editing

--- public/sw.js
+++ public/sw.js
@@ -29,8 +29,7 @@ self.addEventListener('fetch', function(event) {
         event.respondWith(caches.match('./'));
         return;
     }
-    /*   
-    */
+
     event.respondWith(
         caches.match(event.request).then(function(response) {
             if (response) {
