74035691c9414d40af104798b479d135bf5efc57
jotasprout <jotasprout@gmail.com>
2018-08-23 21:58:11 -0400

    starting offline reviews db

--- public/js/dbhelper.js
+++ public/js/dbhelper.js
@@ -216,4 +216,50 @@ class DBHelper {
     });
   } 
 
+  static manageOfflineReviews(callback) {
+
+    /* Creates local db and object store for reviews written offline */
+    const dbPromise = idb.open ('review-db', 1, function (upgradeDb) {
+      const store = upgradeDb.createObjectStore('offlineReviews-store', {
+        keyPath: 'id'
+      });
+    });
+
+    dbPromise.then (function(db) {
+      var tx = db.transaction('offlineReviews-store', 'readwrite');
+      var res = tx.objectStore('offlineReviews-store');
+      return res.getAll();
+    }).then(function(reviews) {
+      // if any reviews are returned from local db call them back
+      if (reviews.length !==0){
+        callback(null, reviews);
+      } else {
+        // if no reviews were returned from local db fetch reviews from server
+        /*
+        fetch (`http://localhost:1337/reviews/`)
+        .then(function(response) {
+          return response.json();
+          */
+        })
+        .then(function(reviews) {
+          // put reviews from server into local db
+
+          dbPromise.then(function(db){
+            var tx = db.transaction('offlineReviews-store', 'readwrite');
+            var res = tx.objectStore('offlineReviews-store');
+            reviews.forEach(
+              review => res.put(review)
+            ); 
+            callback(null, reviews);
+            return tx.complete; 
+          });
+        });
+      }
+    }).then(function(){
+        // console.log("added reviews");
+      }).catch(function(error){
+        console.log(error);
+      }); // end of dbPromise.then       
+  } // end of manageOfflineReviews  
+
 }
